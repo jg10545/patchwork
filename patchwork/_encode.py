82cee1f3f53189f69bef6ec2458342db51ed9859
joe <joegezo@gmail.com>
2019-05-19 10:07:57 -0400

    context encoder has multiple inputs/outputs, fixed masked L2 loss

--- patchwork/_encode.py
+++ patchwork/_encode.py
@@ -43,31 +43,36 @@ def build_decoder(layers=[256, 128, 64, 32, 32], inpt_size=(8,8,512)):
 
 
 def build_context_encoder():
-    N = 256
-    mask_start = int(N/4)
-    mask = np.zeros((N,N,3), dtype=bool)
-    mask[mask_start:3*mask_start, mask_start:3*mask_start,:] = True
+    #N = 256
+    #mask_start = int(N/4)
+    #mask = np.zeros((N,N,3), dtype=bool)
+    #mask[mask_start:3*mask_start, mask_start:3*mask_start,:] = True
     
-    def masked_l2_loss(y_true, y_pred):
-        m = tf.expand_dims(tf.constant(mask.astype(np.float32)),0)
-        m = tf.layers.flatten(m)
-    
-        return tf.keras.backend.mean(
-            m * tf.square(tf.layers.flatten(y_pred) - tf.layers.flatten(y_true)), axis=-1
-        )
+    #def masked_l2_loss(y_true, y_pred):
+    #    m = tf.expand_dims(tf.constant(mask.astype(np.float32)),0)
+    #    m = tf.layers.flatten(m)
+    #
+    #    return tf.keras.backend.mean(
+    #        m * tf.square(tf.layers.flatten(y_pred) - tf.layers.flatten(y_true)), axis=-1
+    #    )
     
     encoder = build_encoder()
     transition = build_transition()
     decoder = build_decoder()
     
-    inpt = tf.keras.layers.Input((256,256,3))
+    inpt = tf.keras.layers.Input((256,256,3), name="inpt")
+    inpt_mask = tf.keras.layers.Input((256,256,3), name="inpt_mask")
     encoded = encoder(inpt)
     updated = transition(encoded)
     decoded = decoder(updated)
+    
+    masked_decoded = tf.keras.layers.Multiply(name="masked_decoded")(
+                                    [inpt_mask, decoded])
 
-    context_encoder = tf.keras.Model(inpt, decoded)
+    context_encoder = tf.keras.Model([inpt, inpt_mask], 
+                                     [decoded, masked_decoded])
     context_encoder.compile(tf.keras.optimizers.Adam(1e-3),
-                            loss=tf.keras.losses.mse)
+                            loss={"masked_decoded":tf.keras.losses.mse})
     #                   loss=masked_l2_loss)
     
     return context_encoder, encoder
@@ -88,10 +93,12 @@ def train_and_test(filepaths):
     train_x = train_ims.copy()
     train_x[:,mask] = 0
     
-    train_y = train_ims
-    test_y = test_ims
+    train_y = train_ims.copy() # added copy
+    train_y[:,~mask] = 0 # new
+    test_y = test_ims.copy() # added copy
+    test_y[:,~mask] = 0 # new
 
     test_x = test_ims.copy()
     test_x[:,mask] = 0
     
-    return train_x, train_y, test_x, test_y
+    return train_x, train_y, test_x, test_y, mask # added mask
