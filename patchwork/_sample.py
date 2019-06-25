938c6030f0b900d9875a8d76fba3ed55ded85fae
Joe <joegezo@gmail.com>
2019-06-25 13:55:21 -0700

    fixed bug in stratified sampler

--- patchwork/_sample.py
+++ patchwork/_sample.py
@@ -64,42 +64,60 @@ def find_subset(df, s):
 
 
 
-def stratified_sample(df, N=1000):
+def stratified_sample(df, N=1000, return_indices=False):
     """
     Build a stratified sample from a dataset. Maps NAs in partially
     labeled records to -1.
     
     :df: DataFrame containing file paths (in a "filepath" column) and
         labels in other columns
+    :N: number of samples
+    :return_indices: whether to return indices instead of file paths
+    
+    Returns
+    (filepaths or indices), label vectors
     """
-    #if N is None:
-    #    N = len(df)
     index = df.index.values
+    not_excluded = df["exclude"] == False
     filepaths = df["filepath"].values
     label_types = [x for x in df.columns if x not in ["filepath", "exclude"]]
-
+    
+    # build a hierarchical set of lists for sampling:
+    # outer list has two elements: negative and positive labels
+    # each of those lists has one list per class, so long as that class has
+    # the right type of labels
+    # each element of those is an array of indices meeting the class/label criteria
     file_lists = [[
-            index[df[l] == 0] \
-            for l in label_types if (df[l] == 0).sum() > 0
+            index[(df[l] == 0)&not_excluded] \
+            for l in label_types if (df[l][not_excluded] == 0).sum() > 0
             ],
             [
-            index[df[l] == 1] \
-            for l in label_types if (df[l] == 1).sum() > 0
+            index[(df[l] == 1)&not_excluded] \
+            for l in label_types if (df[l][not_excluded] == 1).sum() > 0
             ]]
     num_lists = [len(file_lists[0]), len(file_lists[1])]
     
     outlist = []
     ys = []
+    inds = []
     for n in range(N):
+        # choose to sample a positive or negative label
         z = np.random.choice([0,1])
+        # choose a category with positive/negative
         i = np.random.choice(np.arange(num_lists[z]))
-        outlist.append(filepaths[np.random.choice(file_lists[z][i])])
-        #y_vector = label_df.loc[z].values.astype(float)
-        y_vector = df[label_types].loc[z].values.astype(float)
+        # choose an index consistent with i and z
+        ind = np.random.choice(file_lists[z][i])
+        inds.append(ind)
+        outlist.append(filepaths[ind])
+        # convert labels to a vector with None mapped to -1
+        y_vector = df[label_types].loc[ind].values.astype(float)
         y_vector[np.isnan(y_vector)] = -1
         ys.append(y_vector.astype(int))
         
-    return outlist, np.stack(ys)
+    if return_indices:
+        return np.array(inds), np.stack(ys)
+    else:
+        return outlist, np.stack(ys)
 
 
 def unlabeled_sample(df, N=1000):
