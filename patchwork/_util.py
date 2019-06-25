78193911eb5902f2e9661a0478c28164922c0752
Joe <joegezo@gmail.com>
2019-06-25 11:01:18 -0700

    added geotiff loader

--- patchwork/_util.py
+++ patchwork/_util.py
@@ -1,4 +1,5 @@
 import numpy as np
+from osgeo import gdal
 
 def shannon_entropy(x):
     """
@@ -6,3 +7,24 @@ def shannon_entropy(x):
     """
     xprime = np.maximum(np.minimum(x, 1-1e-8), 1e-8)
     return -np.sum(xprime*np.log2(xprime), axis=1)
+
+
+
+def tiff_to_array(f, swapaxes=True, norm_thousand=False, channels=-1):
+    """
+    Utility function to load a GeoTIFF to a numpy array
+    """
+    infile = gdal.Open(f)
+    im_arr = infile.ReadAsArray()
+    if swapaxes:
+        im_arr = np.swapaxes(im_arr, 0, -1)
+    if norm_thousand:
+        im_arr = im_arr.astype(np.float32)/1000
+        
+    if channels > 0:
+        if channels == 1:
+            im_arr = im_arr[:,:,0]
+        else:
+            im_arr = im_arr[:,:,:channels]
+        
+    return im_arr
