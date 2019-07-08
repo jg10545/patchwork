ce032bcf21f06c388033cc16c2f50b5454daec54
Joe <joegezo@gmail.com>
2019-07-07 22:17:47 -0400

    macro to build label dataframes

---
+++ patchwork/_prep.py
@@ -0,0 +1,22 @@
+"""
+
+            _prep.py
+
+
+"""
+
+import pandas as pd
+
+
+def prep_label_dataframe(filepaths, classes):
+    """
+    Build a pandas DataFrame for labeling
+    
+    :filepaths: list of strings; paths to each image file
+    :classes: list of strings; name of each category to label
+    """
+    df = pd.DataFrame({"filepath":filepaths})
+    df["exclude"] = False
+    for c in classes:
+        df[c] = None
+    return df
