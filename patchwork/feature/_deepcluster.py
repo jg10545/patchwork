41a31817a8ff209a5d859091cd7b6603229be870
joe <joegezo@gmail.com>
2019-07-14 23:34:50 -0400

    tweaks to deepcluster code

--- patchwork/feature/_deepcluster.py
+++ patchwork/feature/_deepcluster.py
@@ -51,7 +51,8 @@ def _build_model(feature_extractor, imshape=(256,256), num_channels=3,
     for d in dense:
         net = tf.keras.layers.Dropout(0.5)(net)
         net = tf.keras.layers.Dense(d, activation="relu")(net)
-    output_layer = tf.keras.layers.Dense(k, activation="softmax", name="softmax_out")
+    output_layer = tf.keras.layers.Dense(k, activation="softmax", 
+                                         name="softmax_out")
     output = output_layer(net)
     
     model = tf.keras.Model(inpt, net)
@@ -70,7 +71,7 @@ def _build_model(feature_extractor, imshape=(256,256), num_channels=3,
 def train_deepcluster(filepaths, feature_extractor=None, logdir=None, 
                       epochs=10, pca_dim=256, k=1000, 
                       batch_size=256, mult=10, imshape=(256,256), 
-                      num_channels=3, num_parallel_calls=2):
+                      num_channels=3, num_parallel_calls=2, dense=[]):
     """
     Train a DeepCluster model.
     
@@ -78,19 +79,25 @@ def train_deepcluster(filepaths, feature_extractor=None, logdir=None,
     :feature_extractor: fully-convolutional keras model. If None, build a modified
         AlexNet like what was used in the Deepcluster paper
     :logdir: string; path to directory to write training logs in
-    :num_epochs: number of epochs for training
+    :epochs: number of epochs for training
     :pca_dim: dimension to reduce network outputs to using PCA
     :k: number of clusters for k-means
     :batch_size: batch size
     :mult: not in paper; multiplication factor to increase
         number of steps/epoch. set to 1 to get paper algorithm
+    :imshape: shape to resize images to
+    :num_channels: number of input channels of images
+    :num_parallel_calls: parallelization for input pipeline
+    :dense: list of ints for dense layers between feature extractor and
+        softmax prediction. set to [4096,4096] to reproduce Caron et al's model
     """
     # no FCN is passed- build one
     if feature_extractor is None:
         feature_extractor = BNAlexNetFCN(num_channels)
     # build model for training    
     model, training_model, output_layer = _build_model(feature_extractor, imshape=imshape, 
-                         num_channels=num_channels, dense=[4096,4096], k=k)
+                         num_channels=num_channels, dense=[], k=k)
+    #[4096,4096]
 
     if logdir is not None:
         callbacks = [
