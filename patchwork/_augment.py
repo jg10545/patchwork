8c0ee49fc0f37cb25b395e0b83192a62e1f63d03
joe <joegezo@gmail.com>
2020-01-04 15:13:23 -0500

    augment zoom function can now zoom out as well as in

--- patchwork/_augment.py
+++ patchwork/_augment.py
@@ -16,44 +16,70 @@ DEFAULT_AUGMENT_PARAMS = {
         "left_right_flip":True, 
         "up_down_flip":True,
         "rot90":True,
-        "zoom_scale":0.2,
+        "zoom_scale":0.3,
         "select_prob":0.5
         }
 
 
+def _poisson(lam):
+    return tf.random.poisson(shape=[], lam=lam, 
+                               dtype=tf.int32)
+
 def _random_zoom(im, imshape, scale=0.1):
-    # choose random box values
+    """
+    Randomly pad and then randomly crop an image
+    """
+    im = tf.expand_dims(im, 0)
+    
+    # first, pad the image
+    toppad = _poisson(imshape[0]*scale/2)
+    leftpad = _poisson(imshape[1]*scale/2)
+    bottompad = _poisson(imshape[0]*scale/2)
+    rightpad = _poisson(imshape[1]*scale/2)
+    padded = tf.image.pad_to_bounding_box(
+        im,
+        toppad,
+        leftpad,
+        toppad+imshape[0]+bottompad,
+        leftpad+imshape[1]+rightpad
+    )
+    
+    # second, crop the image
     xmin = tf.random.uniform(shape=[], minval=0, maxval=scale)
     xmax = tf.random.uniform(shape=[], minval=1-scale, maxval=1)
     ymin = tf.random.uniform(shape=[], minval=0, maxval=scale)
     ymax = tf.random.uniform(shape=[], minval=1-scale, maxval=1)
     box = tf.stack([[ymin, xmin, ymax, xmax]])
 
-    resized = tf.image.crop_and_resize(tf.expand_dims(im, 0), 
+    resized = tf.image.crop_and_resize(padded, 
                                        box, 
                                        [0],
                                        imshape)
     return tf.squeeze(resized, axis=0)
+
     
-def _augment(im, imshape=None, max_brightness_delta=0.2, contrast_min=0.4, contrast_max=1.4,
-             max_hue_delta=0.1,  max_saturation_delta=0.5,
-             left_right_flip=True, up_down_flip=True, rot90=True, zoom_scale=0.1,
+def _choose(prob):
+    return tf.random.uniform(()) <= prob
+
+def _augment(im, imshape=None, max_brightness_delta=False, contrast_min=0.4, contrast_max=False,
+             max_hue_delta=False,  max_saturation_delta=False,
+             left_right_flip=False, up_down_flip=False, rot90=False, zoom_scale=False,
              select_prob=0.5):
     """
     Macro to do random image augmentation
     """
     # built-in methods
     if max_brightness_delta:
-        if tf.random.uniform(()) <= select_prob:
+        if _choose(select_prob):
             im = tf.image.random_brightness(im, max_brightness_delta)
     if bool(contrast_min) & bool(contrast_max):
-        if tf.random.uniform(()) <= select_prob:
+        if _choose(select_prob):
             im = tf.image.random_contrast(im, contrast_min, contrast_max)
     if max_hue_delta:
-        if tf.random.uniform(()) <= select_prob:
+        if _choose(select_prob):
             im = tf.image.random_hue(im, max_hue_delta)
     if max_saturation_delta:
-        if tf.random.uniform(()) <= select_prob:
+        if _choose(select_prob):
             im = tf.image.random_saturation(im, 1-max_saturation_delta,
                                         1+max_saturation_delta)
     if left_right_flip:
@@ -65,16 +91,8 @@ def _augment(im, imshape=None, max_brightness_delta=0.2, contrast_min=0.4, contr
             theta = tf.random.uniform(shape=[], minval=0, maxval=4, dtype=tf.int32)
             im = tf.image.rot90(im, theta)
     if zoom_scale > 0:
-        if tf.random.uniform(()) <= select_prob:
+        if _choose(select_prob):
             im = _random_zoom(im, imshape, zoom_scale)
-    
-    # custom- random rotation, distortion, crop, and rescale
-    #if rotate_degrees:
-    #    im = _random_rotate(im)
-    #if distort_theta:
-    #    im = _random_distort(im, distort_theta)
-    #if crop_delta:
-    #    im = _random_crop(im, crop_delta)
         
     # some augmentation can put values outside unit interval
     im = tf.minimum(im, 1)
@@ -88,8 +106,8 @@ def augment_function(imshape, params=True):
     """
     if params == True:
         params = DEFAULT_AUGMENT_PARAMS
-    #return lambda x: _augment(x, **params)
     @tf.function
     def _aug(x):
         return _augment(x, imshape=imshape, **params)
     return _aug
+
