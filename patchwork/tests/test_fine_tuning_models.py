a20598008fd5b517e5604f77be5d4c93f8f979d1
Joe <joegezo@gmail.com>
2020-02-10 14:01:26 -0500

    unit tests

---
+++ patchwork/tests/test_fine_tuning_models.py
@@ -0,0 +1,74 @@
+import numpy as np
+import tensorflow as tf
+from patchwork._fine_tuning_models import GlobalPooling, ConvNet
+
+
+
+def test_GlobalPooling_with_maxpool():
+    pool = GlobalPooling()
+    model = pool.build(2)
+    
+    testarr = np.zeros((1,5,7,2))
+    testarr[0,:,:,1] = 1
+    
+    test_out = model(testarr).numpy()
+    
+    assert isinstance(model, tf.keras.Model)
+    assert len(model.layers) == 2
+    assert (test_out == np.array([[0.,1.]])).all()
+    
+    
+    
+def test_GlobalPooling_with_avpool():
+    pool = GlobalPooling()
+    pool.pooling_type = "average pool"
+    model = pool.build(2)
+    
+    testarr = np.zeros((1,5,4,2))
+    testarr[0,:,:2,1] = 1
+    
+    test_out = model(testarr).numpy()
+    
+    assert isinstance(model, tf.keras.Model)
+    assert len(model.layers) == 2
+    assert (test_out == np.array([[0.,0.5]])).all()
+    
+    
+def test_ConvNet():
+    c = ConvNet()
+    
+    c.dropout_rate = 0
+    c.number_of_layers = 2
+    c.filters_per_layer = 5
+    model = c.build(7)
+    
+    assert isinstance(model, tf.keras.Model)
+    assert len(model.layers) == 4
+    assert model.output_shape[1] == 5
+    
+    
+def test_ConvNet_with_dropout():
+    c = ConvNet()
+    
+    c.dropout_rate = 0.5
+    c.number_of_layers = 2
+    c.filters_per_layer = 5
+    model = c.build(7)
+    
+    assert isinstance(model, tf.keras.Model)
+    assert len(model.layers) == 6
+    assert model.output_shape[1] == 5
+    
+def test_ConvNet_with_separable_convolutions():
+    c = ConvNet()
+    
+    c.dropout_rate = 0
+    c.number_of_layers = 2
+    c.filters_per_layer = 5
+    c.separable_convolutions = True
+    model = c.build(7)
+    
+    assert isinstance(model, tf.keras.Model)
+    assert len(model.layers) == 4
+    assert model.output_shape[1] == 5
+    
