fixed annoying bug in sobel augmentation, removed rms gradient tensorboard callback for SimCLR

patchwork/_augment.py
@@ -6,6 +6,7 @@
 """
 import numpy as np
 import tensorflow as tf
+import tensorflow.keras.backend as K
 
 
 
@@ -67,7 +68,7 @@ def _sobelize(x, prob=0.1, **kwargs):
         # rescale for contrast
         maxval = tf.reduce_max(x)
         minval = tf.reduce_min(x)
-        x = (x - minval)/(maxval-minval)
+        x = (x - minval)/(maxval-minval + K.epsilon())
         # stack back to original number of channels
         x = tf.stack(num_channels*[x], -1)
     return x

patchwork/feature/_simclr.py
@@ -112,11 +112,9 @@ def _build_simclr_training_step(embed_model, optimizer, temperature=0.1):
         gradients = tape.gradient(loss, embed_model.trainable_variables)
         optimizer.apply_gradients(zip(gradients,
                                       embed_model.trainable_variables))
-        rms_grads = 0
-        for g in gradients:
-            rms_grads += tf.math.sqrt(tf.reduce_mean(g**2))
+
         avg_cosine_sim = tf.reduce_mean(sim)
-        return loss, rms_grads, avg_cosine_sim
+        return loss, avg_cosine_sim
     return training_step
 
 
@@ -253,9 +251,9 @@ class SimCLRTrainer(GenericExtractor):
         
         """
         for x, y in self._ds:
-            loss, rms_grads, avg_cosine_sim = self._training_step(x,y)
+            loss, avg_cosine_sim = self._training_step(x,y)
             
-            self._record_scalars(loss=loss, rms_gradients=rms_grads, 
+            self._record_scalars(loss=loss,
                                  avg_cosine_sim=avg_cosine_sim)
             self.step += 1
              

patchwork/feature/_simclr_multi.py
@@ -46,24 +46,18 @@ def _build_distributed_training_step(strategy, embed_model, optimizer,
                 optimizer.apply_gradients(zip(gradients,
                                       embed_model.trainable_variables))
                 
-                rms_grads = 0
-                for g in gradients:
-                    rms_grads += tf.math.sqrt(tf.reduce_mean(g**2))
                 avg_cosine_sim = tf.reduce_mean(sim)
                 return crossent, rms_grads, avg_cosine_sim
         
-        per_example_losses, rms_grads, avg_cos = strategy.experimental_run_v2(
+        per_example_losses, avg_cos = strategy.experimental_run_v2(
                                         step_fn, args=(x,y))
         total_loss = strategy.reduce(
                         tf.distribute.ReduceOp.SUM, 
                         per_example_losses, axis=0)
-        rms_grads = strategy.reduce(
-                        tf.distribute.ReduceOp.MEAN, 
-                        rms_grads, axis=None)
         avg_cos = strategy.reduce(
                         tf.distribute.ReduceOp.MEAN, 
                         avg_cos, axis=None)
-        return total_loss, rms_grads, avg_cos
+        return total_loss,  avg_cos
     return train_step
 
 
@@ -142,7 +136,6 @@ class DistributedSimCLRTrainer(SimCLRTrainer):
                         "full":embed_model}
         
         # build training dataset
-        global_batch_size = batch_size*strategy.num_replicas_in_sync
         self._ds = strategy.experimental_distribute_dataset(
                 _build_simclr_dataset(trainingdata, 
                                       imshape=imshape, batch_size=batch_size,
@@ -177,8 +170,7 @@ class DistributedSimCLRTrainer(SimCLRTrainer):
                                         num_parallel_calls=num_parallel_calls, 
                                         norm=norm, num_channels=num_channels, 
                                         augment=augment,
-                                        single_channel=single_channel,
-                                        global_batch_size=global_batch_size)
+                                        single_channel=single_channel)
             
             @tf.function
             def test_loss(x,y):

patchwork/tests/test_feature_simclr.py
@@ -69,7 +69,7 @@ def test_build_simclr_training_step():
     
     x = tf.zeros((4,32,32,3), dtype=tf.float32)
     y = np.array([1,-1,1,-1]).astype(np.int32)
-    loss, rms_grads, avg_cosine_sim = step(x,y)
+    loss, avg_cosine_sim = step(x,y)
     
     assert isinstance(loss.numpy(), np.float32)
     