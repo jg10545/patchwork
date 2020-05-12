ff385afceb41a05ff8d1c6d0114f49fe304de3f0
joe <joegezo@gmail.com>
2020-05-11 23:34:40 -0400

    not sure whether this is worth it or not

--- patchwork/feature/_simclr.py
+++ patchwork/feature/_simclr.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import tensorflow as tf
+from tqdm import tqdm
 
 from patchwork.feature._generic import GenericExtractor
 from patchwork._augment import augment_function
@@ -164,8 +165,47 @@ def _build_simclr_training_step(embed_model, optimizer, temperature=0.1,
     return training_step
 
 
+def random_adjust_aug_params(a, sigma=0.05):
+    new_aug = {}
+    for k in a:
+        if isinstance(a[k], float):
+            new_aug[k] = round(min(max(a[k]+np.random.normal(0,sigma),0),1), 2)
+        else:
+            new_aug[k] = a[k]
+    return new_aug
 
 
+def find_new_aug_params(trainer, testfiles, num_trials=25, sigma=0.05):
+    aug_params = trainer.augment_config
+    
+    @tf.function
+    def test_loss(x,y):
+        eye = tf.linalg.eye(y.shape[0])
+        index = tf.range(0, y.shape[0])
+        labels = index+y
+
+        embeddings = trainer._models["full"](x)
+        embeds_norm = tf.nn.l2_normalize(embeddings, axis=1)
+        sim = tf.matmul(embeds_norm, embeds_norm, transpose_b=True)
+        logits = (sim - BIG_NUMBER*eye)/trainer.config["temperature"]
+            
+        loss = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(labels, logits))
+        return loss
+    
+    results = []
+
+    for i in tqdm(range(num_trials)):
+        loss = 0
+        aug = random_adjust_aug_params(aug_params, sigma)
+        ds = _build_simclr_dataset(testfiles, augment=aug,
+                                   **trainer.input_config)
+        for x, y in ds:
+            loss += test_loss(x,y).numpy()
+        
+        results.append((aug, loss))
+    argmax = np.argmax([x[1] for x in results])
+    return results[argmax][0], results
 
 
 class SimCLRTrainer(GenericExtractor):
