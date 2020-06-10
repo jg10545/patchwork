2d9306b08abd83ba7eec31f27e5976aecefacbbd
Joe <joegezo@gmail.com>
2020-06-10 12:54:21 -0400

    added focal loss to GUI

--- patchwork/tests/test_losses.py
+++ patchwork/tests/test_losses.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 
 from patchwork._losses import entropy_loss, masked_binary_crossentropy
 from patchwork._losses import masked_mean_average_error
+from patchwork._losses import masked_binary_focal_loss
 
 def test_entropy_loss():
     bs = 7
@@ -27,7 +28,22 @@ def test_masked_binary_crossentropy():
     mbc = masked_binary_crossentropy(y_true, y_pred)
         
     assert isinstance(mbc, tf.Tensor)
-    assert np.sum(mbc.numpy()) < 1e-5
+    assert mbc.numpy() < 1e-5
+    
+
+def test_masked_focal_loss():
+    y_true = np.array([-1., -1, 0., 1.], dtype=np.float32)
+    y_pred = np.array([1., 0., 0.25, 0.75], dtype=np.float32)
+    
+    mbc = masked_binary_crossentropy(y_true, y_pred)
+    focal_loss_gamma_0 = masked_binary_focal_loss(y_true, y_pred, 0)
+    focal_loss_gamma_2 = masked_binary_focal_loss(y_true, y_pred, 2)
+        
+    assert isinstance(focal_loss_gamma_0, tf.Tensor)
+    # at gamma=0, focal loss collapses to crossentropy loss
+    assert abs(mbc.numpy()-focal_loss_gamma_0.numpy()) < 1e-5
+    # at higher gamma values the loss is pushed down
+    assert mbc.numpy() > focal_loss_gamma_2.numpy()
     
     
 def test_masked_mean_average_error():
