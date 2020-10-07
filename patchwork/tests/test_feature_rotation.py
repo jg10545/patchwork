8fb0ae714ca5f4f807604a5607abca39b4a0a8f3
Joe <joegezo@gmail.com>
2020-10-06 21:42:39 -0400

    started adding unit tests

---
+++ patchwork/tests/test_feature_rotation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+
+from patchwork.feature._rotation import _rotate, _build_rotation_dataset
+from patchwork.feature._rotation import _build_rotation_training_step
+
+
+    
+def test_build_rotation_dataset(test_png_path):
+    filepaths = 10*[test_png_path]
+    ds = _build_rotation_dataset(filepaths, imshape=(32,32),
+                                     batch_size=5, 
+                                     augment={"flip_left_right":True})
+    assert isinstance(ds, tf.data.Dataset)
+    for x,y in ds:
+        x = x.numpy()
+        y = y.numpy()
+        break
+    
+    assert x.shape == (5,32,32,3)
+    assert y.shape == (5,)
+    assert y.max() < 4
+    assert y.min() >= 0
+    
