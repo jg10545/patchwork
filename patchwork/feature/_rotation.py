c1c6dd094e0b34d338c0621f45bd48637f474549
Joe <joegezo@gmail.com>
2020-10-06 20:48:37 -0400

    first shot at a rotation trainer

---
+++ patchwork/feature/_rotation.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+
+from patchwork.feature._generic import GenericExtractor
+from patchwork._augment import augment_function
+from patchwork.loaders import _image_file_dataset
+from patchwork._util import compute_l2_loss
+
+
+
+def _rotate(x, foo=False, **kwargs):
+    # create a labeled rotated image
+    theta = tf.random.uniform(shape=[], minval=0, maxval=4, dtype=tf.int32)
+    return tf.image.rot90(x, theta), theta
+
+
+
+
+def _build_rotation_dataset(imfiles, imshape=(256,256), batch_size=256, 
+                      num_parallel_calls=None, norm=255,
+                      num_channels=3, augment=False,
+                      single_channel=False):
+    """
+    """
+    ds = _image_file_dataset(imfiles, shuffle=True, augment=augment, 
+                             imshape=imshape, norm=norm,
+                             single_channel=single_channel,
+                             num_parallel_calls=num_parallel_calls)
+        
+    ds = ds.map(_rotate, num_parallel_calls=num_parallel_calls)
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(1)
+    return ds
+    
+
+
+
+def _build_rotation_training_step(model, optimizer, weight_decay=0):
+    """
+    
+    """
+    
+    @tf.function
+    def training_step(x,y):
+        
+        with tf.GradientTape() as tape:
+            z = model(x, training=True)
+            loss = tf.reduce_mean(
+                tf.keras.losses.sparse_categorical_crossentropy(y, z)
+                )
+            
+            if weight_decay > 0:
+                loss += weight_decay*compute_l2_loss(model)
+    
+        # update fast model
+        variables = model.trainable_variables
+        gradients = tape.gradient(loss, variables)
+        optimizer.apply_gradients(zip(gradients, variables))        
+        return {"loss":loss}
+    return training_step
+
+
+
+
+
+class RotationTrainer(GenericExtractor):
+    """
+
+    
+    """
+
+    def __init__(self, logdir, trainingdata, testdata=None, fcn=None, 
+                 augment={"flip_left_right":True}, 
+                 num_hidden=2048, weight_decay=0,
+                 lr=0.01, lr_decay=100000, decay_type="exponential",
+                 opt_type="momentum",
+                 imshape=(256,256), num_channels=3,
+                 norm=255, batch_size=64, num_parallel_calls=None,
+                 single_channel=False, notes="",
+                 downstream_labels=None):
+        """
+        :logdir: (string) path to log directory
+        :trainingdata: (list) list of paths to training images
+        :testdata: (list) filepaths of a batch of images to use for eval
+        :fcn: (keras Model) fully-convolutional network to train as feature extractor
+        :augment: (dict) dictionary of augmentation parameters, True for defaults
+        :weight_decay: L2 loss weight; 0 to disable
+        :lr: (float) initial learning rate
+        :lr_decay: (int) number of steps for one decay period (0 to disable)
+        :decay_type: (string) how to decay the learning rate- "exponential" (smooth exponential decay), "staircase" (non-smooth exponential decay), or "cosine"
+        :opt_type: (str) which optimizer to use; "momentum" or "adam"
+        :imshape: (tuple) image dimensions in H,W
+        :num_channels: (int) number of image channels
+        :norm: (int or float) normalization constant for images (for rescaling to
+               unit interval)
+        :batch_size: (int) batch size for training
+        :num_parallel_calls: (int) number of threads for loader mapping
+        :single_channel: if True, expect a single-channel input image and 
+                stack it num_channels times.
+        :notes: (string) any notes on the experiment that you want saved in the
+                config.yml file
+        :downstream_labels: dictionary mapping image file paths to labels
+        """
+        self.logdir = logdir
+        self.trainingdata = trainingdata
+        self._downstream_labels = downstream_labels
+        
+        self._file_writer = tf.summary.create_file_writer(logdir, flush_millis=10000)
+        self._file_writer.set_as_default()
+        
+        # if no FCN is passed- build one
+        with self.scope():
+            if fcn is None:
+                fcn = tf.keras.applications.ResNet50V2(weights=None, include_top=False)
+            self.fcn = fcn
+            # make a prediction model 
+            inpt = tf.keras.layers.Input((None, None, num_channels))
+            net = fcn(inpt)
+            net = tf.keras.layers.Flatten()(net)
+            net = tf.keras.layers.Dense(num_hidden, activation="relu")(net)
+            net = tf.keras.layers.Dense(4, activation="softmax")(net)
+            full_model = tf.keras.Model(inpt, net)
+            self._models = {"fcn":fcn, 
+                        "full":full_model}
+        
+        # build training dataset
+        self._ds = _build_rotation_dataset(trainingdata, 
+                            imshape=imshape, batch_size=batch_size, 
+                            num_parallel_calls=num_parallel_calls, 
+                            norm=norm, num_channels=num_channels, 
+                            augment=augment, single_channel=single_channel)
+        
+        # create optimizer
+        self._optimizer = self._build_optimizer(lr, lr_decay, opt_type=opt_type,
+                                                decay_type=decay_type)
+        
+        
+        # build training step
+        self._training_step = _build_rotation_training_step(
+                full_model, 
+                self._optimizer, weight_decay)
+        # build evaluation dataset
+        #if testdata is not None:
+        #    self._test_ds, self._test_steps = dataset(testdata,
+        #                             imshape=imshape,norm=norm,
+        #                             sobel=sobel, num_channels=num_channels,
+        #                             single_channel=single_channel)
+        #    self._test = True
+        #else:
+        #    self._test = False
+        self._test = False
+        self._test_labels = None
+        self._old_test_labels = None
+        
+        # build prediction dataset for clustering
+
+        self.step = 0
+        self._step_var = tf.Variable(0, dtype=tf.int64)
+        
+        
+        # parse and write out config YAML
+        self._parse_configs(augment=augment,  
+                            num_hidden=num_hidden, weight_decay=weight_decay,
+                            lr=lr, lr_decay=lr_decay, opt_type=opt_type,
+                            imshape=imshape, num_channels=num_channels,
+                            norm=norm, batch_size=batch_size,
+                            num_parallel_calls=num_parallel_calls, 
+                            single_channel=single_channel, notes=notes,
+                            trainer="rotation")
+        self._prepopulate_buffer()
+        
+        
+    def _run_training_epoch(self, **kwargs):
+        """
+        
+        """
+        for x, y in self._ds:
+            lossdict = self._training_step(x,y, self._step_var)
+            
+            self._record_scalars(**lossdict)
+            self._record_scalars(learning_rate=self._get_current_learning_rate())
+            
+            self.step += 1
+            
+ 
+    def evaluate(self):
+        # image visualization for the buffer- can probably
+        # drop this if we're confident it's working
+        #b = tf.expand_dims(tf.expand_dims(self._buffer,0),-1)
+        #self._record_images(buffer=b)
+            
+        if self._downstream_labels is not None:
+            # choose the hyperparameters to record
+            if not hasattr(self, "_hparams_config"):
+                from tensorboard.plugins.hparams import api as hp
+                hparams = {
+                    hp.HParam("num_hidden", hp.IntInterval(1, 1000000)):self.config["num_hidden"],
+                    hp.HParam("weight_decay", hp.RealInterval(0., 10000.)):self.config["weight_decay"]
+                    }
+                for k in self.augment_config:
+                    if isinstance(self.augment_config[k], float):
+                        hparams[hp.HParam(k, hp.RealInterval(0., 10000.))] = self.augment_config[k]
+            else:
+                hparams=None
+            self._linear_classification_test(hparams)
+        
+        
+            
+        
+
