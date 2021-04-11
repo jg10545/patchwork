cc49308672e74ff48e115b23ca4dc291185ca0b7
Joe <joegezo@gmail.com>
2021-04-11 15:51:32 -0400

    improved detcon visualizer

--- patchwork/viz/_detcon.py
+++ patchwork/viz/_detcon.py
@@ -20,6 +20,8 @@ def detcon_input_pipeline(imfiles, augment, mean_scale=1000, num_samples=16,
     ds = _image_file_dataset(imfiles, shuffle=False, imshape=imshape,
                              **kwargs).prefetch(1)
     # place to store some examples
+    imgs = []
+    segs = []
     img1s = []
     img2s = []
     seg1s = []
@@ -27,7 +29,7 @@ def detcon_input_pipeline(imfiles, augment, mean_scale=1000, num_samples=16,
 
     N = len(imfiles)
     not_enough_segs_count = 0
-    skipped_for_bad_segmentation = 0
+    augment_removed_segmentation = 0
     
     aug2 = {k:augment[k] for k in augment if k not in SEG_AUG_FUNCTIONS}
     _aug = augment_function(imshape, aug2)
@@ -54,37 +56,58 @@ def detcon_input_pipeline(imfiles, augment, mean_scale=1000, num_samples=16,
         # check to see if any segments were pushed out of the image by augmentation
         segmentation_ok = _filter_out_bad_segments(img1, seg1, img2, seg2)
         if not segmentation_ok:
-            skipped_for_bad_segmentation += 1
+            augment_removed_segmentation += 1
         
-        else:
-            # finally, augment images separately
+        # finally, augment images separately
             
-            img1 = _aug(img1).numpy()
-            img2 = _aug(img2).numpy()
-            seg1 = seg1.numpy()
-            seg2 = seg2.numpy()
-            if len(img1s) < 16:
-                img1s.append(img1)
-                img2s.append(img2)
-                seg1s.append(seg1)
-                seg2s.append(seg2)
+        img1 = _aug(img1).numpy()
+        img2 = _aug(img2).numpy()
+        seg1 = seg1.numpy()
+        seg2 = seg2.numpy()
+        if len(img1s) < 16:
+            imgs.append(x)
+            segs.append(seg)
+            img1s.append(img1)
+            img2s.append(img2)
+            seg1s.append(seg1)
+            seg2s.append(seg2)
         progressbar.update()
     
+    img = np.stack(imgs, 0)
+    seg = np.stack(segs, 0)
     img1 = np.stack(img1s, 0)
     img2 = np.stack(img2s, 0)
     seg1 = np.stack(seg1s, 0)
     seg2 = np.stack(seg2s, 0)
     progressbar.close()
     print(f"Had to sample with replacement for {not_enough_segs_count} of {N} images")
-    print(f"Had to skip {skipped_for_bad_segmentation} of {N} images due to augmentation")
+    print(f"At least one missing segment in {augment_removed_segmentation} of {N} images due to augmentation")
 
-    for j in range(8):
-        plt.subplot(4,4,2*j+1)
+    def _segshow(s):
+        plt.imshow(s, alpha=0.4, extent=[0,imshape[0],imshape[1],0], 
+                   cmap="tab20", vmin=0, vmax=num_samples-1)
+
+    for j in range(5):
+        plt.subplot(5,4,4*j+1)
+        plt.imshow(img[j])
+        plt.axis(False)
+        if j == 0: plt.title("original")
+        
+        plt.subplot(5,4, 4*j+2)
+        plt.imshow(img[j])
+        plt.axis(False)
+        _segshow(seg[j].argmax(-1))
+        if j == 0: plt.title("segments")
+        
+        plt.subplot(5,4, 4*j+3)
         plt.imshow(img1[j])
-        plt.imshow(seg1[j].argmax(-1), alpha=0.4, extent=[0,imshape[0],imshape[1],0], cmap="tab20")
-        plt.axis(False);
-
-        plt.subplot(4,4,2*j+2)
+        plt.axis(False)
+        _segshow(seg1[j].argmax(-1))
+        if j == 0: plt.title("augmentation 1")
+        
+        plt.subplot(5,4, 4*j+4)
         plt.imshow(img2[j])
-        plt.imshow(seg2[j].argmax(-1), alpha=0.4, extent=[0,imshape[0],imshape[1],0], cmap="tab20")
-        plt.axis(False);
+        plt.axis(False)
+        _segshow(seg2[j].argmax(-1))
+        if j == 0: plt.title("augmentation 2");
+        
