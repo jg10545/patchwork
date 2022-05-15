e0562f98a717d4d5b71240407853e02ef4d07038
Joe <joegezo@gmail.com>
2022-05-15 08:20:25 -0400

    code cleanup

--- patchwork/_optimizers.py
+++ patchwork/_optimizers.py
@@ -57,24 +57,7 @@ class CosineDecayWarmup(tf.keras.optimizers.schedules.LearningRateSchedule):
             return tf.cond(step <= self.warmup_steps,
                            lambda: self._warmupstep(step),
                            lambda: self._decaystep(step))
-        """
-            if step < self.warmup_steps:
-                return self.initial_learning_rate*step/self.warmup_steps
-            else:
-                step -= self.warmup_steps
-                initial_learning_rate = ops.convert_to_tensor_v2_with_dispatch(
-                    self.initial_learning_rate, name="initial_learning_rate")
-                dtype = initial_learning_rate.dtype
-                decay_steps = math_ops.cast(self.decay_steps, dtype)
-            
-                global_step_recomp = math_ops.cast(step, dtype)
-                global_step_recomp = math_ops.minimum(global_step_recomp, decay_steps)
-                completed_fraction = global_step_recomp / decay_steps
-                cosine_decayed = 0.5 * (1.0 + math_ops.cos(
-                    constant_op.constant(math.pi) * completed_fraction))
         
-            decayed = (1 - self.alpha) * cosine_decayed + self.alpha
-            return math_ops.multiply(initial_learning_rate, decayed)"""
 
     def get_config(self):
         return {
