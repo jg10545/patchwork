5c8310516d18eb6dd1236e4d42a075d0554d6849
Joe <joegezo@gmail.com>
2022-08-10 21:48:48 -0400

    head2toe first implementation

--- patchwork/_fine_tuning_models.py
+++ patchwork/_fine_tuning_models.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
 import param
 import tensorflow as tf
+import re
 
 from patchwork._layers import _next_layer
+
+
+def _build_multi_output_fcn(oldfcn, layers, pooling="average pool"):
+    """
+    Take a Keras feature extractor and
+    """
+    if pooling == "average pool":
+        pool = tf.keras.layers.GlobalAveragePooling2D
+    elif pooling == "max pool":
+        pool = tf.keras.layers.GlobalMaxPool2D
+    else:
+        assert False, "what kind of pooling is this?"
+    # regex case
+    if isinstance(layers, str):
+        layers = [e for e, l in enumerate(oldfcn.layers)
+                  if len(re.findall(layers, l.name)) > 0]
+    outputs = []
+    inpt = oldfcn.input
+    for l in layers:
+        net = oldfcn.layers[l].output
+        net = pool()(net)
+        outputs.append(net)
+
+    return tf.keras.Model(inpt, outputs)
 
 
 class GlobalPooling(param.Parameterized):
@@ -10,12 +35,12 @@ class GlobalPooling(param.Parameterized):
     Just a single pooling layer.
     """
     pooling_type = param.ObjectSelector(default="max pool", objects=["max pool", "average pool", "flatten"])
-    
+
     description = """
     A single pooling or flattening layer to map outputs of a feature extractor to a dense vector. No trainable parameters.
     """
-    
-    def build(self, feature_shape):
+
+    def build(self, feature_shape, feature_extractor):
         #inpt = tf.keras.layers.Input((None, None, inpt_channels))
         inpt = tf.keras.layers.Input(feature_shape)
         if self.pooling_type == "max pool":
@@ -26,15 +51,15 @@ class GlobalPooling(param.Parameterized):
             pool = tf.keras.layers.Flatten()
         # store a reference to the model in case we need it later
         self._model = tf.keras.Model(inpt, pool(inpt), name="finetuning")
-        return self._model
-    
+        return self._model, feature_extractor
+
     def model_params(self):
         return {"fine_tuning_type":"GlobalPooling",
                 "pooling_type":self.pooling_type,
                 "num_params":self._model.count_params(),
                 "num_layers":len(self._model.layers)}
-        
-        
+
+
 class ConvNet(param.Parameterized):
     """
     Convolutional network
@@ -43,20 +68,20 @@ class ConvNet(param.Parameterized):
     kernel_size = param.ObjectSelector(default=1, objects=[1,3,5,7], doc="Spatial size of filters")
     batchnorm = param.Boolean(False, doc="Whether to use batch normalization in convolutional layers")
     separable_convolutions = param.Boolean(False, doc="Whether to use depthwise separable convolutions")
-    dropout_rate = param.Number(0.5, bounds=(0.05,0.95), doc="Spatial dropout rate.")
-    pooling_type = param.ObjectSelector(default="max pool", objects=["max pool", "average pool", "flatten"], 
+    dropout_rate = param.Number(0.5, bounds=(0.05,0.95), step=0.05, doc="Spatial dropout rate.")
+    pooling_type = param.ObjectSelector(default="max pool", objects=["max pool", "average pool", "flatten"],
                                         doc="Whether to use global mean or max pooling.")
-    
-    
+
+
     _description = """
     Convolutional network with global pooling at the end. Set dropout to 0 to disable.
     """
     description = """
-    Convolutional network with global pooling at the end. \n\n    
+    Convolutional network with global pooling at the end. \n\n
     Use a comma-separated list to define layers: integer for a convolution, `p` for 2x2 max pooling, `d` for 2D spatial dropout, and `r` for a residual block.
     """
-    
-    def build(self, feature_shape):
+
+    def build(self, feature_shape, feature_extractor):
         inpt = tf.keras.layers.Input(feature_shape)
         net = inpt
         for l in self.layers.split(","):
@@ -65,7 +90,7 @@ class ConvNet(param.Parameterized):
                               dropout_rate=self.dropout_rate,
                               separable=self.separable_convolutions,
                               batchnorm=self.batchnorm)
-            
+
         if self.pooling_type == "max pool":
             net = tf.keras.layers.GlobalMaxPool2D()(net)
         elif self.pooling_type == "average pool":
@@ -74,8 +99,8 @@ class ConvNet(param.Parameterized):
             net = tf.keras.layers.Flatten()(net)
         # store reference to model in case we need it later
         self._model = tf.keras.Model(inpt, net, name="finetuning")
-        return self._model
-    
+        return self._model, feature_extractor
+
     def model_params(self):
         return {"fine_tuning_type":"ConvNet",
                 "pooling_type":self.pooling_type,
@@ -84,4 +109,57 @@ class ConvNet(param.Parameterized):
                 "kernel_size":self.kernel_size,
                 "separable":self.separable_convolutions,
                 "structure":self.layers}
-        
+
+
+class MultiscalePooling(param.Parameterized):
+    """
+    Concatenate pooled features from multiple layers
+    """
+    pooling_type = param.ObjectSelector(default="average pool", objects=["max pool", "average pool"])
+    layers = param.String(default="add", doc="Comma-separated list of filters")
+    use_dropout = param.Boolean(default=True, doc="Use layerwise dropout")
+    dropout_rate = param.Number(0.5, bounds=(0.05, 0.95), step=0.05, doc="Spatial dropout rate.")
+
+    description = """
+    Loosely based on *Head2Toe: Utilizing Intermediate Representations for Better Transfer Learning* by Evci *et al.*
+    Uses layerwise dropout instead of group LASSO. No trainable parameters.
+    """
+
+    def build(self, feature_shape, feature_extractor):
+        # rebuild the feature extractor to output results at each layer.
+        # first parse the layer list
+        try:
+            layers = [int(x) for x in self.layers.split(",")]
+        except:
+            layers = self.layers
+        # rebuild feature extractor
+        multi_output_fcn = _build_multi_output_fcn(feature_extractor,
+                                                   layers,
+                                                   self.pooling_type)
+
+        # build new finetuning model to accept multiple inputs
+        inputshapes = [l.shape[-1] for l in multi_output_fcn.outputs]
+        inputs = []
+        outlayers = []
+        for i in inputshapes:
+            inpt = tf.keras.layers.Input(i)
+            net = inpt
+            if self.use_dropout:
+                net = tf.keras.layers.Reshape([i, 1])(net)
+                net = tf.keras.layers.SpatialDropout1D(self.dropout_rate)(net)
+                net = tf.keras.layers.Reshape([i])(net)
+            inputs.append(inpt)
+            outlayers.append(net)
+        net = tf.keras.layers.Concatenate()(outlayers)
+        self._model = tf.keras.Model(inputs, net, name="finetuning")
+        return self._model, multi_output_fcn
+
+    def model_params(self):
+        return {"fine_tuning_type": "MultiscalePooling",
+                "pooling_type": self.pooling_type,
+                "num_params": self._model.count_params(),
+                "num_layers": len(self._model.layers),
+                "feature_layers": self.layers,
+                "dropout":self.use_dropout,
+                "dropout_rate":self.dropout_rate}
+
--- patchwork/tests/test_fine_tuning_models.py
+++ patchwork/tests/test_fine_tuning_models.py
@@ -1,69 +1,94 @@
 import numpy as np
 import tensorflow as tf
-from patchwork._fine_tuning_models import GlobalPooling, ConvNet
+from patchwork._fine_tuning_models import GlobalPooling, ConvNet, MultiscalePooling
 
 
 
 def test_GlobalPooling_with_maxpool():
     pool = GlobalPooling()
-    model = pool.build((None, None, 2))
-    
+    model, _ = pool.build((None, None, 2), None)
+
     testarr = np.zeros((1,5,7,2))
     testarr[0,:,:,1] = 1
-    
+
     test_out = model(testarr).numpy()
-    
+
     assert isinstance(model, tf.keras.Model)
     assert len(model.layers) == 2
     assert (test_out == np.array([[0.,1.]])).all()
-    
-    
-    
+
+
+
 def test_GlobalPooling_with_avpool():
     pool = GlobalPooling()
     pool.pooling_type = "average pool"
-    model = pool.build((None, None, 2))
-    
+    model, _ = pool.build((None, None, 2), None)
+
     testarr = np.zeros((1,5,4,2))
     testarr[0,:,:2,1] = 1
-    
+
     test_out = model(testarr).numpy()
-    
+
     assert isinstance(model, tf.keras.Model)
     assert len(model.layers) == 2
     assert (test_out == np.array([[0.,0.5]])).all()
-    
-    
+
+
 def test_ConvNet():
     c = ConvNet()
-    
+
     c.layers = "10,p,11"
-    model = c.build((None, None, 7))
-    
+    model, _ = c.build((None, None, 7), None)
+
     assert isinstance(model, tf.keras.Model)
     assert len(model.layers) == 7
     assert model.output_shape[1] == 11
-    
-    
+
+
 def test_ConvNet_with_dropout():
     c = ConvNet()
-    
+
     c.dropout_rate = 0.5
     c.layers = "7,p,d,11,p,d,13"
-    model = c.build((None, None, 7))
-    
+    model, _ = c.build((None, None, 7), None)
+
     assert isinstance(model, tf.keras.Model)
     assert len(model.layers) == 12
     assert model.output_shape[1] == 13
-    
+
 def test_ConvNet_with_separable_convolutions():
     c = ConvNet()
 
     c.layers = "5,13"
     c.separable_convolutions = True
-    model = c.build((None, None, 7))
-    
+    model, _ = c.build((None, None, 7), None)
+
     assert isinstance(model, tf.keras.Model)
     assert len(model.layers) == 6
     assert model.output_shape[1] == 13
-    
+
+
+def test_multiscalepooling_with_layer_index_list():
+    inpt = tf.keras.layers.Input((None, None, 3))
+    net = tf.keras.layers.Conv2D(3, 1, name="foo")(inpt)
+    net = tf.keras.layers.Conv2D(5, 1, name="bar")(net)
+    net = tf.keras.layers.Conv2D(7, 1, name="foobar")(net)
+    testfcn = tf.keras.Model(inpt, net)
+
+    mp = MultiscalePooling()
+    mp.layers = "1,2"
+    finetuning, newfcn = mp.build((None, None, 7), testfcn)
+    assert finetuning.output_shape[-1] == 3 + 5
+
+
+def test_multiscalepooling_with_layer_name_regex():
+    inpt = tf.keras.layers.Input((None, None, 3))
+    net = tf.keras.layers.Conv2D(3, 1, name="foo")(inpt)
+    net = tf.keras.layers.Conv2D(5, 1, name="bar")(net)
+    net = tf.keras.layers.Conv2D(7, 1, name="foobar")(net)
+    testfcn = tf.keras.Model(inpt, net)
+
+    mp = MultiscalePooling()
+    mp.layers = "foo"
+    finetuning, newfcn = mp.build((None, None, 7), testfcn)
+    assert finetuning.output_shape[-1] == 3 + 7
